95f7f627b2d61a52427e0ad9cb7382983064e198
Samuel Lamy <samuel.lamy@umontreal.ca>
2022-02-07 20:14:48 -0500

    classe_LOC

---
+++ src/TP1/Main.java
@@ -0,0 +1,26 @@
+package TP1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\Samuel\\IdeaProjects\\ift3913-tp1\\src\\TP1\\Main.java");
+        Scanner sc = new Scanner(file);
+
+        System.out.println(classe_LOC(sc));
+    }
+
+    private static int classe_LOC(Scanner sc){
+        int count = 0;
+        String ligne;
+        while (sc.hasNext()) {
+            ligne = sc.nextLine();
+            if(!ligne.equals("")){
+                count++;
+            }
+        }
+        return count;
+    }
+}
